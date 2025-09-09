adee611647854b3093d5bb36eb310c4f901f003c
Dayanand Jakkali <dayanandjakkali64@gmail.com>
2025-09-09 19:36:41 +0530

    Create TypesOfVariable.java
    Instance variables are declared inside the class but outside any method.
    
    String name; → default value: null
    
    int age; → default value: 0
    
    float height; → default value: 0.0
    
    Since no values are assigned, Java will print their default values.

---
+++ TypesOfVariable.java
@@ -0,0 +1,20 @@
+
+public class TypesOfVariable {
+
+    // Instance variables
+    String name;
+    int age;
+    float height;
+
+    public static void main(String[] args) {
+        TypesOfVariable s = new TypesOfVariable();
+        System.out.println(s.name);
+        System.out.println(s.age);
+        System.out.println(s.height);
+    }
+}
+
+Answer:null
+0
+0.0
+
